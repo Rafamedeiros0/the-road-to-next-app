c2176d41c7dcbde1418ece15dd8737a39dd9dce1
Rafael Medeiros <rafamedeiros0@gmail.com>
2025-10-29 11:03:20 -0600

    feat(ticket): add ticket creation form

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,12 +3,30 @@ import { Suspense } from "react";
 import { Heading } from "@/components/heading";
 // import { PlaceholderV2 } from "@/components/placeholderV2";
 import { Spinner } from "@/components/spinner";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
+
+      <Card className="w-full max-w-[420px] self-center">
+        <CardHeader>
+          <CardTitle>Create Ticket</CardTitle>
+          <CardDescription>A new ticket will be created</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <TicketCreateForm></TicketCreateForm>
+        </CardContent>
+      </Card>
 
       {/* <ErrorBoundary fallback={<PlaceholderV2 title="Something went wrong!" />}> */}
       <Suspense fallback={<Spinner />}>
