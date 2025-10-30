518123ee842c7e92c08682205017a0f641edbf13
Rafael Medeiros <rafamedeiros0@gmail.com>
2025-10-29 23:43:04 -0600

    feat: add authentication with better-auth

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/react";
+import { getBaseUrl } from "@/utils/url";
+
+export const authClient = createAuthClient({
+  baseURL: getBaseUrl(),
+});
+
+// export const { signIn, signUp, useSession } = createAuthClient();
