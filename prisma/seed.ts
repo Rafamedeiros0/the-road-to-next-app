d1f5e1c7a6a5c13dd83b71d9fdc0d25495f54bff
Rafael Medeiros <rafamedeiros0@gmail.com>
2025-10-30 13:57:39 -0600

    feat(db): add cascade delete for user tickets and optimize seed order

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -42,10 +42,8 @@ const seed = async () => {
   const t0 = performance.now();
   console.log("DB Seed: Started...");
 
-  await prisma.ticket.deleteMany();
-  await prisma.account.deleteMany();
-  await prisma.session.deleteMany();
   await prisma.user.deleteMany();
+  await prisma.ticket.deleteMany();
 
   const passwordHash = await hashPassword("12345678");
 
